97ae116b56e878f3c5e0bcfc30031aef7ef68e67
JosephToledano <joseph.a.toledano@gmail.com>
2021-03-04 12:10:50 -0500

    Committing changes in order to merge

--- client/app.tsx
+++ client/app.tsx
@@ -3,17 +3,16 @@ import UserIcon from "./components/UserIcon";
 import UserBadgeDropdown from "./components/UserBadgeDropdown";
 import MapWrapper from "./components/MapContainer";
 import TripDetailsForm from "./components/TripDetailsForm/TripDetailsForm";
-import NewUserPopup from "./components/NewUserPopup";
 
 const App = () => {
   const [currentSel, setCurrentSel] = useState("");
   const [visited, setVisited] = useState<string[]>([]);
   const [currentUser, setCurrentUser] = useState<string>("");
-  const [showPopUp, setShowPopUp] = useState<boolean>(true);
+
 
   return (
     <>
-      {showPopUp ? <NewUserPopup setShowPopUp={setShowPopUp} /> : null}
+      {}
       <UserIcon
         setCurrentUser={setCurrentUser}
         currentUser={currentUser}
